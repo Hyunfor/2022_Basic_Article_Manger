ada5f4d8dcf7eb76e0e9a8ba2ade1c0699186e46
Hyunfor <navel5148@gmail.com>
2022-09-16 20:07:57 +0900

    게시물 삭제 시도중(id: x , index : o)

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -1,6 +1,8 @@
 package com.KoreaIT.java.BAM;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,6 +17,9 @@ public class Main {
 		int lastArticleId = 0;
 
 		List<Article> articles = new ArrayList<>();
+		
+		Date date = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		
 		while(true) {
 			System.out.printf("명령어 ) ");
@@ -80,13 +85,10 @@ public class Main {
 					continue;
 				} else { // 일치하는 게시물이 존재하는 경우	
 					System.out.printf("번호 : %d \n", foundArticle.id);
-					System.out.printf("날짜 : %s \n", "2022-12-12 12:12:12");
+					System.out.printf("날짜 : %s \n", formatter.format(date));
 					System.out.printf("제목 : %s \n", foundArticle.title);
 					System.out.printf("내용 : %s \n", foundArticle.body);
 				}
-
-
-
 				
 			} else if (cmd.startsWith("article delete ")) {
 				String[] cmdBits = cmd.split(" ");
@@ -99,7 +101,6 @@ public class Main {
 					
 					if(article.id == id) { // 명령어에 입력한 id가 일치한다면
 						foundArticle = article; 
-						System.out.printf("%d번 게시글은 삭제합니다.\n", id);
 						break;
 					}
 				}
@@ -108,6 +109,9 @@ public class Main {
 					System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
 					continue;
 				}
+				
+				articles.remove(id - 1); // 삭제
+				System.out.printf("%d번 게시물이 삭제되었습니다.\n", id); // 삭제가 된다면 출력
 				
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다.");
