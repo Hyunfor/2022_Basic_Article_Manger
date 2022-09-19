8a5edeb7ef36da602866b0eb656b029102329b7d
Hyunfor <navel5148@gmail.com>
2022-09-19 21:27:54 +0900

    리팩토링, 패키지 분리, 핵심 로직 이전

---
+++ src/com/KoreaIT/java/BAM/dto/Article.java
@@ -0,0 +1,27 @@
+package com.KoreaIT.java.BAM.dto;
+
+public class Article {
+	public int id;
+	public String regDate;
+	public String title;
+	public String body;
+	public int viewCnt;
+	
+	public Article (int id, String regDate, String title, String body){ // 생성자
+		this(id, regDate, title, body, 0); // 다른 생성자에게 일을 떠넘김
+	}
+	
+	// 함수명이 똑같지만 매개변수, 인자의 차이로 쓰는것이 오버로딩
+	public Article(int id, String regDate, String title, String body, int viewCnt) {
+		this.id = id;
+		this.regDate = regDate;
+		this.title = title;
+		this.body = body;
+		this.viewCnt = viewCnt;
+	}
+
+	public void addViewCnt(){ // 조회수 증가
+		viewCnt++;
+	}
+	
+}
