736bbef8257cbc8449ec5765031e9caf53df486b
Hyunfor <navel5148@gmail.com>
2022-09-21 19:40:07 +0900

    회원가입 기능 구현

--- src/com/KoreaIT/java/BAM/App.java
+++ src/com/KoreaIT/java/BAM/App.java
@@ -42,21 +42,46 @@ public class App {
 				int id = members.size() + 1;
 				String regDate = Util.getNowDateStr();
 				
-				System.out.printf("아이디 : ");
-				String loingId = sc.nextLine();
-				System.out.printf("비밀번호 : ");
-				String loginPw = sc.nextLine();
-				System.out.printf("비밀번호 확인 : ");
-				String loginPwChk = sc.nextLine();
+				String loginId = null;
+				while(true) {
+					System.out.printf("로그인 아이디 : ");
+					loginId = sc.nextLine();
+					
+					if(loginIdChk(loginId) == false) { // 
+						System.out.printf("%s은(는) 이미 사용중인 아이디입니다.\n", loginId);
+						continue;
+					}
+					System.out.printf("%s은(는) 사용가능한 아이디입니다.\n", loginId);
+					break;
+				}
+				
+				
+				String loginPw = null;
+				String loginPwChk = null;
+				while(true) {
+					System.out.printf("로그인 비밀번호 : ");
+					loginPw = sc.nextLine();
+					System.out.printf("비밀번호 확인 : ");
+					loginPwChk = sc.nextLine();
+					
+					if(loginPw.equals(loginPwChk) == false) { 
+						// 로그인 비밀번호와 비밀번호 확인이 일치할시 실행, 틀리면 false 실행
+						System.out.println("비밀번호를 다시 입력해주세요.");
+						continue;
+					}
+					break; // 비밀번호가 일치할시 반복문 탈출
+				}
+				
+				
 				System.out.printf("이름 : ");
 				String name = sc.nextLine();
 
 				
-				Member member = new Member(id, regDate, loingId, loginPw, name);
+				Member member = new Member(id, regDate, loginId, loginPw, name);
 				
 				members.add(member); // join 할때마다 게시글을 하나씩 배열에 저장
 				
-				System.out.printf("%s 회원님 환영합니다 \n", loingId);
+				System.out.printf("%s 회원님 환영합니다 \n", loginId);
 				
 			} else if (cmd.equals("article write")) {
 				int id = articles.size() + 1;
@@ -202,6 +227,28 @@ public class App {
 		sc.close();
 	}
 	
+	private boolean loginIdChk(String loginId) {
+		int index = getMemberIndexByLoginId(loginId); // 먼저 실행 후
+		
+		if(index == -1) { // 아이디 중복이 없을 경우 true
+			return true;
+		}
+		
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+		for(Member member : members) { 
+			
+			if(member.loginId.equals(loginId)) { // 문자열은 equals로 
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+
 	private int getArticleIndexById(int id) {
 		int i = 0;
 		for(Article article : articles) { 
